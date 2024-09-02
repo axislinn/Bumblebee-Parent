d8774285088eefdac7f007de23636e9d618626cc
axislinn <linnwaiyan2424@gmail.com>
2024-09-02 17:49:48 +0630

    login successful

--- lib/bloc/bloc/login_bloc.dart
+++ lib/bloc/bloc/login_bloc.dart
@@ -8,7 +8,6 @@ import 'login_event.dart';
 import 'login_state.dart';
 
 
-
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   final UserRepository userRepository;
 
--- lib/data/repository/repositories/user_repository.dart
+++ lib/data/repository/repositories/user_repository.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -7,32 +8,35 @@ class UserRepository {
   final String baseUrl = 'https://bumblebeeflutterdeploy-production.up.railway.app';
 
   // Method for login
-  Future<UserModel> authenticate({required String email, required String password}) async {
-    final url = Uri.parse('$baseUrl/api/auth/login'); // Updated to match provided function
-    //final url = Uri.parse('$baseUrl/http://192.168.100.179:3000/userLogin');
-    //final url = Uri.parse('http://192.168.100.179:3000/userLogin');
+Future<UserModel> authenticate({required String email, required String password}) async {
+  final url = Uri.parse('$baseUrl/api/auth/login'); // Updated to match provided function
 
-    final response = await http.post(
-      url,
-      headers: {'Content-Type': 'application/json'},
-      body: json.encode({
-        'email': email,
-        'password': password,
-      }),
-    );
+  final response = await http.post(
+    url,
+    headers: {'Content-Type': 'application/json'},
+    body: json.encode({
+      'email': email,
+      'password': password,
+    }),
+  );
 
-    if (response.statusCode == 200) {
-      var jsonResponse = json.decode(response.body);
-      if (jsonResponse['status']) {
-        // Use UserModel for further user handling
-        return UserModel.fromJson(jsonResponse['data']);
-      } else {
-        throw Exception('Failed to login: ${jsonResponse['message']}');
-      }
+  if (response.statusCode == 200) {
+    var jsonResponse = json.decode(response.body);
+    print('response is ' + response.body);
+
+    // Ensure jsonResponse['status'] is not null and is a boolean
+    if (jsonResponse['status'] != null && jsonResponse['status'] is bool && jsonResponse['status']) {
+      // Use UserModel for further user handling
+      return UserModel.fromJson(jsonResponse['data']);
     } else {
-      throw Exception('Failed to login: ${response.body}');
+      // Handle cases where 'status' is not a boolean or is false
+      throw Exception('Failed to login: ${jsonResponse['msg']}');
     }
+  } else {
+    throw Exception('Failed to login: ${response.body}');
   }
+}
+
 
   // Method for register
   Future<UserModel> register({
